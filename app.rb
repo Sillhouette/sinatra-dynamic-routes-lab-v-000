3fd0521426f5a762be1a6f5f5adf00e2a1e1fc22
Richard Melchior <raustin.melchior@gmail.com>
2018-06-20 20:58:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,14 +13,34 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-
+    @num = params[:number]
+    @phrase = params[:phrase]
+    "#{@phrase*@num}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"
   end
 
   get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @num1 = params[:number1]
+    @num2 = params[:number2]
 
+    case @operation.downcase
+      when "add"
+        "#{@num1 + @num2}"
+      when "subtract"
+        "#{@num1 - @num2}"
+      when "multiply"
+        "#{@num1 * @num2}"
+      when "divide"
+        "#{@num1 / @num2}"
+    end
   end
 end
